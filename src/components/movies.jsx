9a8f95413c1b3b26d45334fd8a325e39d50bc733
hrk <hrkbrahmbhatt@gmail.com>
2019-05-10 21:38:50 -0400

    Added Like Component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like"
 
 class Movies extends Component {
   state = {
     movies: getMovies()
   };
 
+  handleLike = movie =>{
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index]= { ...movies[index]};
+    movies[index].liked = !movies[index].liked;
+    this.setState({movies});
+    //console.log("Liked worked", movie);
+    
+  }
   handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id != movie._id);
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
   };
   render() {
@@ -33,6 +43,7 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td><Like liked={movie.liked} onLiked={() => this.handleLike(movie)}/></td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
